b583a94ab088ff221860b0eef0c502c3bf421f3b
Julian Hoever <julianhoever@posteo.de>
2022-10-04 18:09:35 +0200

    update example according to recent changes of the update_leds method

--- examples/running_light.py
+++ examples/running_light.py
@@ -5,14 +5,12 @@ from pixel import Pixel
 
 
 def main() -> None:
-    strip = LEDStrip(leds=3, auto_update=False, baudrate=10000)
-    strip.update_leds()
+    strip = LEDStrip(leds=3)
 
     while True:
         for i in range(len(strip)):
-            strip[i] = Pixel(0, 255, 0)
+            strip[i] = Pixel(255, 0, 32)
             strip[i - 1] = Pixel(0, 0, 0)
-            strip.update_leds()
             sleep(0.5)
 
 
